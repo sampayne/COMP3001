7e264d7fbb7122a9e5378db17fbcb042bc805f6b
Ruxandra Panainte <ruxandra-giorgiana.panainte.13@ucl.ac.uk>
2015-11-12 15:34:16 +0000

    Spotted bug in plotting. Added correlation between variables and goodness-of-fit test with chi-squared.

--- implementation/RScripts/linerregpm25temp.R
+++ implementation/RScripts/linerregpm25temp.R
@@ -41,10 +41,16 @@ model = lm(value ~ temperature_c, data=df_pm25_temp_042014)
 summary(model)
 
 #plot
-plot(df_pm25_temp_042014)
+plot(df_pm25_temp_042014$temperature_c, df_pm25_temp_042014$value)
 abline(model)
 
 
 #predict
 df_predicted_week1062015 <- predict(model, newdata = df_pm25_temp_week1052014)
 cbind(df_pm25_temp_week1052014 ,df_predicted_week1062015)
+
+#correlation between variables
+cor(df_pm25_temp_042014$temperature_c, df_pm25_temp_042014$value)
+
+#goodness of fit (chi-squared)
+chisq.test(df_pm25_temp_week1052014$temperature_c, p = df_predicted_week1062015/sum(df_predicted_week1062015))
