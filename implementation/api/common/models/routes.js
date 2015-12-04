5e69e53f823a42a53467600d1fd725835227621a
Vino Mano <vino.m@me.com>
2015-12-04 00:22:36 +0000

    normalizing pollution index and improvements

--- implementation/api/common/models/routes.js
+++ implementation/api/common/models/routes.js
@@ -196,46 +196,210 @@ module.exports = function(Routes) {
 
 		]);
 
+		function Linear(AQIhigh, AQIlow, Conchigh, Conclow, Concentration) {
+		    var linear;
+		    var Conc = parseFloat(Concentration);
+		    var a;
+		    a = ((Conc - Conclow) / (Conchigh - Conclow)) * (AQIhigh - AQIlow) + AQIlow;
+		    linear = Math.round(a);
+		    return linear;
+		}
+
+		function AQIPM25(Concentration) {
+		    var Conc = parseFloat(Concentration);
+		    var c;
+		    var AQI;
+		    c = (Math.floor(10 * Conc)) / 10;
+		    if (c >= 0 && c < 12.1) {
+		        AQI = Linear(50, 0, 12, 0, c);
+		    } else if (c >= 12.1 && c < 35.5) {
+		        AQI = Linear(100, 51, 35.4, 12.1, c);
+		    } else if (c >= 35.5 && c < 55.5) {
+		        AQI = Linear(150, 101, 55.4, 35.5, c);
+		    } else if (c >= 55.5 && c < 150.5) {
+		        AQI = Linear(200, 151, 150.4, 55.5, c);
+		    } else if (c >= 150.5 && c < 250.5) {
+		        AQI = Linear(300, 201, 250.4, 150.5, c);
+		    } else if (c >= 250.5 && c < 350.5) {
+		        AQI = Linear(400, 301, 350.4, 250.5, c);
+		    } else if (c >= 350.5 && c < 500.5) {
+		        AQI = Linear(500, 401, 500.4, 350.5, c);
+		    } else {
+		        AQI = "Out of Range";
+		    }
+		    return AQI;
+		}
+
+		function AQIPM10(Concentration) {
+		    var Conc = parseFloat(Concentration);
+		    var c;
+		    var AQI;
+		    c = Math.floor(Conc);
+		    if (c >= 0 && c < 55) {
+		        AQI = Linear(50, 0, 54, 0, c);
+		    } else if (c >= 55 && c < 155) {
+		        AQI = Linear(100, 51, 154, 55, c);
+		    } else if (c >= 155 && c < 255) {
+		        AQI = Linear(150, 101, 254, 155, c);
+		    } else if (c >= 255 && c < 355) {
+		        AQI = Linear(200, 151, 354, 255, c);
+		    } else if (c >= 355 && c < 425) {
+		        AQI = Linear(300, 201, 424, 355, c);
+		    } else if (c >= 425 && c < 505) {
+		        AQI = Linear(400, 301, 504, 425, c);
+		    } else if (c >= 505 && c < 605) {
+		        AQI = Linear(500, 401, 604, 505, c);
+		    } else {
+		        AQI = "Out of Range";
+		    }
+		    return AQI;
+		}
+
+		function AQINO2(Concentration) {
+		    var Conc = parseFloat(Concentration); //in ug/(m^3) , need to convert into ppb
+		    var divisor = (1.9125 + 1.88)/2;
+		    var oneMGoverM3 = 1/divisor; // ppb
+		    Conc = Conc * oneMGoverM3;
+		    var c;
+		    var AQI;
+		    c = (Math.floor(Conc)) / 1000;
+		    if (c >= 0 && c < .054) {
+		        AQI = Linear(50, 0, .053, 0, c);
+		    } else if (c >= .054 && c < .101) {
+		        AQI = Linear(100, 51, .100, .054, c);
+		    } else if (c >= .101 && c < .361) {
+		        AQI = Linear(150, 101, .360, .101, c);
+		    } else if (c >= .361 && c < .650) {
+		        AQI = Linear(200, 151, .649, .361, c);
+		    } else if (c >= .650 && c < 1.250) {
+		        AQI = Linear(300, 201, 1.249, .650, c);
+		    } else if (c >= 1.250 && c < 1.650) {
+		        AQI = Linear(400, 301, 1.649, 1.250, c);
+		    } else if (c >= 1.650 && c <= 2.049) {
+		        AQI = Linear(500, 401, 2.049, 1.650, c);
+		    } else {
+		        AQI = "Out of Range";
+		    }
+		    return AQI;
+		}
+
+		function normaliseIndex(pollutant,concentrationVal){
+			if(pollutant == "NO2"){
+				return AQINO2(concentrationVal);
+			}else if (pollutant == "PM25"){
+				return AQIPM25(concentrationVal);
+			}else if (pollutant == "PM10"){
+				return AQIPM10(concentrationVal);
+			}else{
+				//unknown 
+				console.log("unknown pollutant");
+				return concentrationVal;
+			}
+		}
+
 		function getPollutionIndex(route){
 			var pollutionVal = 0;
+			var numberOfMissingPollutions = 0;
 			async.forEachOf(route, function(step, key, callback){
 				console.log(step);
-					Routes.app.models.Pollution.findOne({where: { siteId : step.closestSiteId , date : step.time}}, function(err, pollutionItem) {
-						console.log(pollutionItem);
+					Routes.app.models.Pollution.find({where: { siteId : step.closestSiteId , date : step.time}}, function(err, pollutionItems) {
+						console.log("Pollution query results: "+pollutionItems);
 
 			    					if(err) {
 			    						console.log(err);
 			    						callback(err);
 
 			    					}
-			    					if(pollutionItem && !err){
-			    						pollutionVal = pollutionItem.value;
-			    						route[key].pollutionVal = pollutionVal;
-			    						callback(null,0);
+			    					console.log("print is from pollution: "+ pollutionItems)
+			    					if(pollutionItems && !err){
+			    						if(pollutionItems.length > 0){
+			    							var pollutionAvg = 0;
+				    						for(pollutionItem in pollutionItems){
+				    							//pollutionVals.push({'species_id':pollutionItems[pollutionItem].speciesId,'value':pollutionItems[pollutionItem].value});
+				    							//NORMASE HERE FIRST
+				    							var normalisedIndex = normaliseIndex(pollutionItems[pollutionItem].speciesId,pollutionItems[pollutionItem].value);
+				    							pollutionAvg += normalisedIndex;
+				    							console.log("pollutionind: "+normalisedIndex);
+				    						}
+				    						pollutionAvg /= pollutionItems.length;
+				    						console.log("pollutionAvg: "+pollutionAvg);
+
+				    						//route[key].pollutionVal = pollutionVals[0].value;
+				    						route[key].pollutionVal = pollutionAvg;
+				    						//final add averaged and normalised pollution val to route[key]
+				    						callback(null,0);
+
+
+				    					}else{
+				    						Routes.app.models.Prediction.find({where: { siteId : step.closestSiteId , date : step.time}}, function(err1, predictionItems) {
+				    							console.log("Prediction query results: "+predictionItems);
+				    							if(err1) {
+						    						console.log(err1);
+						    						callback(err1);
+						    						return;
+
+						    					}
+						    					console.log("print is from prediction: "+ predictionItems + ", len: "+predictionItems.length)
+
+						    					if(predictionItems && !err1){
+						    						if(predictionItems.length > 0){
+						    							console.log("in here");
+						    							var predictionAvg = 0;
+						    							for(predictionItem in predictionItems){
+						    								//predictionVals.push({'species_id':predictionVals[predictionItem].species_id,'value':predictionVals[predictionItem].value});
+						    								
+						    								console.log("predictionindunnormalised: "+predictionItems[predictionItem].speciesId + ", "+ predictionItems[predictionItem].value);
+
+						    								var normalisedIndex = normaliseIndex(predictionItems[predictionItem].speciesId,predictionItems[predictionItem].value);
+						    								predictionAvg += normalisedIndex;
+						    								console.log("predictionind: "+normalisedIndex);
+
+						    							}
+						    							predictionAvg /= predictionItems.length;
+
+						    							console.log("predictionAvg: "+predictionAvg);
+
+						    							//route[key].pollutionVal = predictionVals[0].value;
+						    							route[key].pollutionVal = predictionAvg;
+						    							callback(null,0);
+
+						    						}else{
+						    							console.log("in here2");
+
+						    							//negative prediction...?
+						    							//check previous value if exists and greater than 0 then use otherwise make 0
+						    							if(key!=0){
+							    							if(route[key-1]){
+							    								if(route[key-1].pollutionVal > 0){
+							    									route[key].pollutionVal = pollutionVal;
+							    								}else{
+							    									route[key].pollutionVal = 0;
+							    								}
+							    							}else{
+							    								route[key].pollutionVal = 0;
+							    							}
+						    							}else{
+						    								//estimated average pollution standard...?
+						    								//route[key].pollutionVal = 0;
+						    								numberOfMissingPollutions++;
+						    							}
+						    							callback(null,0);
+						    						}
+						    					}else{
+						    						console.log("no results/err")
+
+
+						    						callback(pollutionItems,-1);
+						    						return;
+						    					}
+				    						});
+
+				    					}
+			    						
 			    					}else{
 			    						console.log("empty detect");
 			    						//check if it exists in prediction table
-			    						Routes.app.models.Prediction.findOne({where: { siteId : step.closestSiteId , date : step.time}}, function(err1, pollutionItem1) {
-			    							console.log(pollutionItem1);
-			    							if(err1) {
-					    						console.log(err1);
-					    						callback(err1);
-
-					    					}
-					    					if(pollutionItem1 && !err1){
-					    						pollutionVal = pollutionItem1.value;
-					    						route[key].pollutionVal = pollutionVal;
-					    						callback(null,0);
-					    					}else{
-					    						console.log("no results")
-
-
-					    						callback(pollutionItem,-1);
-					    						return;
-					    						//check in prediction table
-					    					}
-			    						});
-
+			    						
 
 
 			    					}
@@ -252,6 +416,8 @@ module.exports = function(Routes) {
 					for(step in route){
 						pollutionIndex += route[step].pollutionVal;
 					}
+
+					pollutionIndex += (pollutionIndex/route.length) * numberOfMissingPollutions; //to account for missing pollutions
 					cb(null,pollutionIndex);
 				}
 			});
